0002ea8de691ef71318c2b0046ba13dff0ff9bb5
Cori1109 <chris.jenkins.j1005@gmail.com>
2022-02-21 07:07:33 +0100

    meta

--- packages/website/src/pages/posts/[slug].tsx
+++ packages/website/src/pages/posts/[slug].tsx
@@ -65,9 +65,9 @@ const PostPage = ({ source, post }: PostPageProps) => {
         <meta name="description" content={post.title} />
         <meta name="og:description" content={post.title} />
         <meta name="twitter:description" content={post.title} />
-        <meta name="og:image" content={IMAGE_PATH + post.coverImage} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={HOME + IMAGE_PATH + post.coverImage} />
+        <meta name="twitter:image" content={`${HOME}${IMAGE_PATH}${post.coverImage}`} />
+        <meta name="og:image" content={`${HOME}${IMAGE_PATH}${post.coverImage}`} />
         <link rel="icon" href="/logo.svg" />
         <meta name="og:title" content={post.title} />
         <meta
